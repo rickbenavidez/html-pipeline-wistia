41352dafab300c604a82f2b43d2baaf5c2f91920
Rick Benavidez <rick.benavidez@gmail.com>
2025-03-19 13:45:15 +0000

    Don't modify the context

--- lib/html/pipeline/wistia/wistia_filter.rb
+++ lib/html/pipeline/wistia/wistia_filter.rb
@@ -9,12 +9,11 @@ module HTML
         @text.gsub(regex) do
           wistia_id = $1
           style = context[:style].presence || ''
-          context[:videoFoam] = 'true'
           # Take all keys in the context hash and convert them to param attributes where key=value and create an array of them
-          params = context.reject { |key, _| key == :style }.map { |key, value| "#{key}=#{value}" }.join('&')
-
+          params = context.reject { |key, _| key == :style }.map { |key, value| "#{key}=#{value}" }
+          params << 'videoFoam=true'
           # Prefix with two "\n" for compatibility with markup such as Markdown:
-          %{\n\n<div class="video wistia" data-wistia-id="#{wistia_id}"><iframe src="//fast.wistia.net/embed/iframe/#{wistia_id}?#{params}" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen width="640" height="360" style="#{style}"></iframe><script src="//fast.wistia.net/assets/external/E-v1.js" async></script></div>}
+          %{\n\n<div class="video wistia" data-wistia-id="#{wistia_id}"><iframe src="//fast.wistia.net/embed/iframe/#{wistia_id}?#{params.join('&')}" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen width="640" height="360" style="#{style}"></iframe><script src="//fast.wistia.net/assets/external/E-v1.js" async></script></div>}
         end
       end
     end
